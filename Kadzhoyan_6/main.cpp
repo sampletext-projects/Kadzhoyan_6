5c6edb963574a088188a7d322b598a060af5c8e9
sampletext32 <sampletext32@bk.ru>
2020-06-19 04:18:26 +0300

    Add project

---
+++ Kadzhoyan_6/main.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+double max(double k, double m)
+{
+	double res;
+	if (k >= m)
+	{
+		res = k;
+	}
+	else
+	{
+		res = m;
+	}
+	return res;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+
+	double x, y;
+
+	cout << "Введите x: ";
+	cin >> x;
+
+	cout << "Введите y: ";
+	cin >> y;
+
+	double t = (max(x, y) - max(x * y, x - y)) / (1.5 + max(x * x, y * y));
+
+	cout << "x: " << x << ", y: " << y << ", t: " << t << endl;
+
+	system("pause");
+
+	return 0;
+}
